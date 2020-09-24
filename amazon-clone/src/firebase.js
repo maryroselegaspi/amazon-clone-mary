2c5f5167085c08dbb9a070e7065de49ce778ffba
Mary <maryrose.legaspi2018@gmail.com>
2020-09-25 07:26:02 +1000

    Adding route

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-TzO-m5756VtYxNTPRq7Fu4eYcXq8wEk",
+    authDomain: "clone-9cc5f.firebaseapp.com",
+    databaseURL: "https://clone-9cc5f.firebaseio.com",
+    projectId: "clone-9cc5f",
+    storageBucket: "clone-9cc5f.appspot.com",
+    messagingSenderId: "371639491726",
+    appId: "1:371639491726:web:e796378f5428a590ac7ddf",
+    measurementId: "G-RSXSR5CVNV"
+  };
+
+  
