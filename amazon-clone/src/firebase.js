cc38ffa03a154b88f1d559e1b020b171c13ccea3
Mary <maryrose.legaspi2018@gmail.com>
2020-10-12 20:50:25 +1000

    Update firebase.js

--- amazon-clone/src/firebase.js
+++ amazon-clone/src/firebase.js
@@ -1,21 +1,22 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyB-TzO-m5756VtYxNTPRq7Fu4eYcXq8wEk",
-    authDomain: "clone-9cc5f.firebaseapp.com",
-    databaseURL: "https://clone-9cc5f.firebaseio.com",
-    projectId: "clone-9cc5f",
-    storageBucket: "clone-9cc5f.appspot.com",
-    messagingSenderId: "371639491726",
-    appId: "1:371639491726:web:e796378f5428a590ac7ddf",
-    measurementId: "G-RSXSR5CVNV"
-  });
+  apiKey: "AIzaSyB-TzO-m5756VtYxNTPRq7Fu4eYcXq8wEk",
+  authDomain: "clone-9cc5f.firebaseapp.com",
+  databaseURL: "https://clone-9cc5f.firebaseio.com",
+  projectId: "clone-9cc5f",
+  storageBucket: "clone-9cc5f.appspot.com",
+  messagingSenderId: "371639491726",
+  appId: "1:371639491726:web:e796378f5428a590ac7ddf",
+  measurementId: "G-RSXSR5CVNV",
+});
 
-  // const db= firebaseApp.firestore();
-  const auth = firebase.auth();
+//Authentication
+const firebaseApp = firebase.initializeApp(firebaseApp);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const storage = firebase.storage();
 
-  export {auth};
-
-
-  
+export { db, auth, storage, firebaseApp };
+// export default db;
