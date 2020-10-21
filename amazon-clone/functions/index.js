f6a52677daa6c1166ddf71890daf296c16ebe0a4
Mary <maryrose.legaspi2018@gmail.com>
2020-10-21 20:47:05 +1000

    backend index.js update

--- amazon-clone/functions/index.js
+++ amazon-clone/functions/index.js
@@ -10,3 +10,33 @@ const functions = require('firebase-functions');
 
 const express = require ('express');
 const cors = require('cors');
+const stripe = require ('stripe')('sk_test_51HcTwABA8zqER3fxpzMBRcZeJBwXS7dZvltwdtcb6mBaj1js0UaxpK9gVmXnQjxleCEkIreU4il6HA8Jf2PVDRR000cSSOZxcg') ;
+
+// API
+
+// App config
+const app = express();
+// Middleware
+app.use(cors({origin:true}));
+app.use(express.json());
+// API routes
+app.get('/',(request, response) => response.status(200).send('Hello There'))
+// app.get('/mary',(request, response) => response.status(200).send('Hey Mary'))
+
+app.post('/payments/create', async(request, response)=> {
+    const total = request.query.total;
+    console.log('payment request:', total)
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount:total, //subunit currency
+        currency:'aud',
+    });
+    response.status(201).send({
+        clientSecret: paymentIntent.client_secret,
+    })
+})
+// List of command
+exports.api= functions.https.onRequest(app)
+
+// Endpoints
+// http://localhost:5001/clone-9cc5f/us-central1/api
